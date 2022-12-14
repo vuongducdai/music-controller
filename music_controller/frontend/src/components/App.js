569d00f387136c145314d1e0dcdb1fd916da9187
Vuong Duc Dai <>
2022-12-14 17:09:35 +0700

    Initial code for frontend

---
+++ music_controller/frontend/src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const App = () => {
+  console.log('Render APp');
+  return <div>Test React Appssssss</div>;
+};
+
+export default App;
---
+++ music_controller/frontend/src/index.js
@@ -0,0 +1,6 @@
+import App from './components/App';
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const app = ReactDOM.createRoot(document.getElementById('app'));
+app.render(<App />);
