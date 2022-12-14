c817b3b4ebf624ecfa690cc3f4e109ebc6a4b49d
Vuong Duc Dai <>
2022-12-14 15:21:57 +0700

    Initial commit for api

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import main
+from .views import RoomView, CreateRoom
 
 urlpatterns = [
-    path('', main )
+    path('all', RoomView.as_view()),
+    path('create', CreateRoom.as_view())
 ]
